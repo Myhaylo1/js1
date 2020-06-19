b44fc226ad3b95139bf92b2518425606fc987b42
Myhaylo1 <zpkazka@gmail.com>
2020-06-19 17:16:03 +0300

    1 commit

---
+++ index.js
@@ -0,0 +1,417 @@
+//window.alert('PrUvet VOLKU!');
+
+// let x = 9;
+// let s = '~~~';
+// console.log(x);
+//
+// console.log('PrUvet VOLKU!');
+// console.log('PrUvet VOLKU!');
+// x=20;
+// console.log(x);
+//
+// let v1;
+// let v2=null;
+// let v3=456.12;
+// let v4='PrUvet VOLKU!';
+// let v5=true;
+// let v6=Symbol();
+//
+// //console.log(typeof v1);
+// //console.log(typeof(v2));
+
+// const a1=7+3;
+// console.log('7+3=',a1);
+// const a2=7-3;
+// console.log('7-3=',a2);
+// const a3=7*3;
+// console.log('7*3=',a3);
+// const a4=7/3;
+// console.log('7/3=',a4);
+// const a5=7%3;
+// console.log('7%3=',a5);
+//
+// console.log('"7"+"3"=','7'+'3',typeof('7'+'3'));
+// console.log('"7"-"3"=','7'-'3');
+// console.log('"7"*"3"=','7'*'3');
+// console.log('"7"/"3"=','7'/'3');
+// console.log('"7"%"3"=','7'%'3');
+//
+// console.log('2+3=',2+3);
+// console.log('"2"+"3"=','2'+'3');
+// console.log('2+"3"=',2+'3');
+// console.log('"2"+3=','2'+3);
+//
+// console.log('2+true=',2+true);
+// console.log('2+undefined=',2+undefined);
+// console.log('"2"+undefined=','2'+undefined);
+// console.log('2+null=',2+null);
+// console.log('"2"+null=','2'+null);
+// //console.log('2+Symbol()=',2+Symbol());
+// //console.log('"2"+Symbol()=','2'+Symbol());
+// console.log('"2"+3.5=','2'+3.5);
+// console.log('"2"+{}}=','2'+ {});
+//
+//
+//
+//
+// console.log('2-3=',2-3);
+// console.log('"2"-"3"=','2'-'3');
+// console.log('2-"3"=',2-'3');
+// console.log('"2"-3=','2'-3);
+//
+// console.log('2-true=',2-true);
+// console.log('2-false=',2-false);
+// console.log('2-undefined=',2-undefined,typeof(2-undefined));
+// console.log('"2"-undefined=','2'-undefined);
+// console.log('2-null=',2-null);
+// console.log('"2"-null=','2'-null);
+// //console.log('2-Symbol()=',2-Symbol());
+// //console.log('"2"-Symbol()=','2'-Symbol());
+// console.log('"2"-3.5=','2'-3.5);
+// console.log('"2"-{}}=','2'- {});
+//
+// console.log('2-true=',2-true);
+// console.log('2-false=',2-false);
+// console.log('2-undefined=',2-undefined,typeof(2-undefined));
+// console.log('"2"-undefined=','2'-undefined);
+// console.log('2-null=',2-null);
+// console.log('"2"-null=','2'-null);
+// //console.log('"2"- Symbol()=','2'-Symbol());
+// //console.log('2-Symbol()=',2-Symbol());
+// console.log('"2"-3.  5=','2'-3.5);
+// console.log('"2"-{}   }=','2'- {});
+//
+// console.log('true/t rue=',true/true);
+//
+// console.log('true/1   4=',true/14);
+// console.log('"81"/t    rue=','81'/true);
+// console.log(4+5+9+' ');
+// console.log('"81"/"    3"=','81'/'3');
+// console.log('true/f  alse=',true/false);
+// console.log('"81"/3    =','81'/3);
+//
+// console.log(10<4    );
+// console.log(10>10   );
+// console.log(10>4    );
+// console.log(10<10   );
+// console.log(10>=10  );
+// console.log(10<=10  );
+// console.log(10==10  );
+// console.log(10==11  );
+// console.log(10===10 );
+// console.log(10===11 );
+// console.log(10!==10 );
+// console.log(10!==11 );
+// console.log(10!===10);
+// console.log(10!===11);
+
+// const p = prompt('Input "4"');
+// if (p==='4') {
+//   alert('!');
+// } else {
+//   alert('...');
+// }
+// console.log('p=',p);
+// alert('');
+// if (true) {
+//   alert('if');
+// }
+
+// console.log(12 == '12');
+// const result = 7-'9 / 3';
+// console.log(result);
+// console.log('7-"3"=',7-'3');
+// console.log('7-"9/3"=',7-'9/3');
+
+// function cube(x)
+// {
+//   return x*x*x;
+// }
+// console.log(cube(2));
+// console.log(12 != '12');
+
+// function isEven(x)
+// {
+//  return x%2===0;
+// }
+// console.log(isEven(2));
+
+// function equation2(a,b,c)
+// {
+//   let x1=NaN;
+//   let x2=NaN;
+//   const d=b*b-4*a*c;
+//   if (d>=0) {
+//      x1=(-b+Math.sqrt(d))/(2*a);
+//      x2=(-b-Math.sqrt(d))/(2*a);
+//   }
+//   return [x1,x2];
+// }
+// console.log(equation2(2,0,3));
+
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ * @returns {number} - max value
+ */
+// function max(a,b)
+// {
+//  if (a>b) {b=a}
+//  return b;
+// }
+// console.log(max(12,5));
+
+// const obj1={};
+// const obj2=new Object();
+//
+// const cat={
+//   name: 'Barsik',
+//   heart:
+//    {
+//     beatSpeed: 100,
+//     chamberCount: 4,
+//     addSpeed: function ()
+//      {
+//       alert('Add speed.');
+//      }
+//    },
+//   sayMeow: function ()
+//    {
+//     alert('Meow!');
+//    }
+// }
+// cat.age=4;
+// //alert(cat.name);
+// console.log(cat);
+//
+// cat.sayMeow();
+
+// function sayHello(){
+//   alert('sayHello');
+// }
+// console.log(typeof sayHello);
+// const testFunc=function (){
+//   alert('testFunc');
+// }
+
+// const car1={
+//   color: 'red',
+//   doorsCount: 5,
+//   engine: {
+//     volume: 1.6,
+//     type: 'ICE'
+//   },
+//   seatsNumber: 8,
+//   maxSpeed: 80,
+//   brand: 'VW',
+//   model: 'Tuareg',
+//   noise: function () {
+//     alert('br-br-br');
+//   },
+// };
+//
+// const car2={
+//   color: 'blue',
+//   doorsCount: 2,
+//   engine: {
+//     volume: 1.0,
+//     type: 'ICE'
+//   },
+//   seatsNumber: 2,
+//   maxSpeed: 60,
+//   brand: 'Liancha',
+//   model: 'Liancha',
+//   noise: function go() {
+//     alert('buh-buh-buh');
+//   },
+// }
+//
+// const car3={
+//   color: 'green',
+//   doorsCount: 2,
+//   engine: {
+//     volume: 1.0,
+//     type: 'ICE'
+//   },
+//   seatsNumber: 5,
+//   maxSpeed: 110,
+//   brand: 'ZAZ',
+//   model: '968M',
+//   noise () {
+//     alert('dr-dr-dr');
+//   },
+// }
+//
+// console.log(car1);
+// car1.noise();
+// console.log(car2);
+// car2.noise();
+// console.log(car3);
+// car3.noise();
+
+// const cat1={
+//   doing: function () {
+//     alert('cat1 is run');
+//   }
+// }
+// cat1.doing();
+//
+// const cat2={
+//   doing: function () {
+//     alert('cat2 is sits');
+//   }
+// }
+// cat2.doing();
+//
+// const cat3={
+//   doing: function () {
+//     alert('cat3 is sleep');
+//   },
+//   doBackFlip
+// }
+// cat3.doing();
+//
+//
+// function doBackFlip(s) {
+//   alert('cat is back flip');
+// }
+//
+// cat2.doBackFlip=doBackFlip;
+//
+// cat3.doBackFlip();
+// cat2.doBackFlip();
+//cat1.doBackFlip();
+
+// const cat4={
+//   doing: function (s) {
+//     alert(s+' is houngree');
+//   },
+//   //doBackFlip
+// }
+// cat4.doing('cat4');
+
+// function User(name, surname) {
+//   this.name=name;
+//   this.surname=surname;
+// }
+// const user1=new User('Petro','Petriv');
+// const user2=new User('Myshko','Myshkiv');
+
+// function Country(name, square, populas) {
+//   this.name=name;
+//   this.square=square;
+//   this.populas=populas;
+//   this.getPopulas=function(){
+//     return this.populas;
+//   }
+// }
+// const country1=new Country(name='Ukraine',650000,40000000);
+// const country2=new Country('Poland',300000,12000000);
+
+// console.log(country1.getPopulas());
+// console.log(country1.getPopulas);
+//
+// function ERS(U, I, R){
+//   this.I=I;
+//   this.R=R;
+//   this.U=U;
+//   this.getU=function () {
+//     return this.I*this.R;
+//   }
+//   this.getI=function () {
+//     return this.U/this.R;
+//   }
+//   this.getR=function() {
+//     return this.U/this.I;
+//   }
+// }
+// const ers=new ERS(60,2,30);
+// console.log(ers.I);
+// console.log(ers.R);
+// console.log(ers.U);
+// ers.U=20;
+// console.log(ers.getI());
+
+// function ERS(I, R){
+//   this.I=I;
+//   this.R=R;
+//   this.U=this.I*this.R;
+//   this.getU=function () {
+//     return this.I*this.R;
+//   }
+//   this.getI=function () {
+//     return this.U/this.R;
+//   }
+//   this.getR=function() {
+//     return this.U/this.I;
+//   }
+// }
+// const ers=new ERS(2,30);
+// console.log(ers.I);
+// console.log(ers.R);
+// console.log(ers.U);
+// ers.U=20;
+// console.log(ers.getI());
+
+//const userInput=prompt('Input');
+// if (userInput==='1'){
+//   alert('=1');
+// } else if (userInput==='2') {
+//   alert('=2');
+// } else if (userInput==='3') {
+//   alert('=3');
+// } else
+// {
+//   alert('???');
+// }
+
+// switch (userInput) {
+//   case '1':{alert('=1');} break;
+//   case '2':{alert('=2');} break;
+//   case '3':{alert('=3');} break;
+//   default: {}
+// }
+
+
+
+
+let operand1=prompt('Input 1st operand');
+if (!isNaN(operand1) && operand1!=='')
+ {
+  operand1=Number(operand1);
+  let operand2 = prompt('Input 2st operand');
+  if (!isNaN(operand2) && operand2!=='')
+  {
+    operand2=Number(operand2);
+    let operand3 = prompt('Choose Your case: 1 - to Add, 2 - to Sub, 3 - to Mul, 4 - to Div');
+    let a;
+    // switch (operand3) {
+    //   case '1': a=add(operand1,operand2); break;
+    //   case '2': a=sub(operand1,operand2); break;
+    //   case '3': a=mul(operand1,operand2); break;
+    //   case '4': a=div(operand1,operand2); break;
+    //  }
+    // if (a!==undefined) alert(a); else alert('Operands are not executable');
+    switch (operand3) {
+      case '1': a=add; break;
+      case '2': a=sub; break;
+      case '3': a=mul; break;
+      case '4': a=div; break;
+     }
+    if (typeof a==='function') alert(a(operand1,operand2)); else alert('Operands are not executable');
+  }
+ }
+
+function add(a,b) {
+  return a+b;
+}
+function sub(a,b) {
+  return a-b;
+}
+function mul(a,b) {
+  return a*b;
+}
+function div(a,b) {
+  if (b) return a/b;
+}
